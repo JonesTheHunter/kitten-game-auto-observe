1fd2da070abfc43c98719121a22cd5bd32881756
daziki <45572109+daziki@users.noreply.github.com>
2025-09-18 20:30:41 -0400

    Added toggle to turn on and off

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-setInterval(() => {
-const observe = document.getElementById('observeButton');
 
-if(observe.children.length > 0) {
-console.log('Observe found')
-    observe.querySelector('input').click()
-} else {
-	console.log('No Observe found.')
-}
-}, 3000);
+const observe = document.getElementById('observeButton');
+const input = document.createElement('input');
+input.setAttribute('type', 'checkbox');
+input.setAttribute('id', 'auto-observe-toggle');
+let autoObserver;
+
+input.addEventListener('change', () => {
+    if(this.checked) {
+            autoObserver = setInterval(() => {
+            
+
+            if(observe.children.length > 0) {
+            console.log('Observe found')
+                observe.querySelector('input').click()
+            } else {
+                console.log('No Observe found.')
+            }
+        }, 3000);
+    } else {
+        clearInterval(autoObserver)
+    }
+})
+observe.insertAdjacentElement('beforebegin', input);
+
+
